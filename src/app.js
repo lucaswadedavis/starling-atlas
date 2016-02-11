b4806e05c3747c263f4c2b20d2f46a481cce6cf1
Luke Davis <luke@peopleofthebit.com>
2016-02-11 15:39:28 -0800

    add a simple intro animation.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 (function(){
 
+  var creds = {
+    id: 'lukedavis.3457dfcb',
+    accessToken: 'pk.eyJ1IjoibHVrZWRhdmlzIiwiYSI6IjcwMDBkNWEyNmZlYzU0YTI0YTYxMGYyMmNkZjBhNjRmIn0.kd__Iir1FCZkgkrp8r-byQ'
+  };
+
   var eid = function(){
     var count = 0;
     eid = function(){
@@ -8,67 +13,29 @@
     return eid();
   }
 
-var app = {locations:[], selectedLocationID:null, map:null};
+  var app = {locations:[], selectedLocationID:null, map:null};
 
-app.displayLocationsList = function(locations){
-  var d = _.reduceRight(locations, function(m,n){
-    var d = "";
-    d += "<div class='well' data-toggle='modal' data-target='#exampleModal' data-coords='"+n.latlng.toString()+"'  data-title='"+n.title+"'  data-notes='"+n.notes+"' data-id='"+n.id+"'>"
-    d += "<h3>"+n.title+"</h3>";
-    d += "<div class='small' >"+n.latlng.toString()+"</div>";
-    d += "<p>"+n.notes+"</p>";
-    d += "</div>";
+  app.templates = {};
+
+  app.templates.locations = function (locations) {
+    return _.reduceRight(locations, function(m,n){
+      var d = "";
+      d += "<div class='well' data-toggle='modal' data-target='#exampleModal' data-coords='"+n.latlng.toString()+"'  data-title='"+n.title+"'  data-notes='"+n.notes+"' data-id='"+n.id+"'>"
+      d += "<h3>"+n.title+"</h3>";
+      d += "<div class='small' >"+n.latlng.toString()+"</div>";
+      d += "<p>"+n.notes+"</p>";
+      d += "</div>";
     return m + d;
-  }, "<h2>Locations</h2>");
+    }, "<h2>Locations</h2>");
+  };
 
-  $("#locations-list").html(d);
-};
+  app.templates.introOverlay = function () {
+    var d = '';
+    d += '<div id="intro-overlay"><h1>The Starling Atlas</h1></div>';
+    return d;
+  };
 
-app.displayMap = function(){
-  var map = L.map('starling-map',{
-    zoomControl: false,
-    attributionControl: false
-  }).setView([37.755817, -122.389932], 11);
-
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      //attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      id: 'lukedavis.3457dfcb',
-      accessToken: 'pk.eyJ1IjoibHVrZWRhdmlzIiwiYSI6IjcwMDBkNWEyNmZlYzU0YTI0YTYxMGYyMmNkZjBhNjRmIn0.kd__Iir1FCZkgkrp8r-byQ'
-  }).addTo(map);
-  
-  /*
-  map.on("click",function(e){
-    var id = eid();
-    var circle = L.circle(e.latlng, 100, {
-      weight:2,
-      color: '#f00',
-      fillOpacity: 0
-    }).addTo(map);
-
-    circle.on("click",function(e){
-      map.removeLayer(circle);
-      for (var i=0;i<app.locations.length;i++){
-        if (app.locations[i].mapObject===circle){
-          app.locations.splice(i,1);
-          break;
-        }
-      }
-      app.displayLocationsList(app.locations);
-    });
-    
-    app.locations.push({id:id, latlng:e.latlng, radius: 100, mapObject: circle, title:"Title", notes:"notes"});
-    app.displayLocationsList(app.locations);
-    
-  });
-  */
-
-  app.map = map;
-
-};
-
-  app.init = function(){
-    app.displayMap();
+  app.listeners = function () {
 
     $('#exampleModal').on('show.bs.modal', function (event) {
       app.selectedLocationID = $(event.relatedTarget).data('id');
@@ -76,9 +43,9 @@ app.displayMap = function(){
       for (var i=0;i<app.locations.length;i++){
         if (app.locations[i].id===app.selectedLocationID){
           var latlng = app.locations[i].latlng;
-          
+
           $(this).find('.modal-title').text(app.locations[i].latlng);
-          $(this).find('#modal-title').val(app.locations[i].title)
+          $(this).find('#modal-title').val(app.locations[i].title);
           $(this).find('#modal-notes').val(app.locations[i].notes);
           app.map.panTo(latlng);
         }
@@ -97,7 +64,83 @@ app.displayMap = function(){
       }
     });
 
+
   };
+
+  app.displayIntro = function () {
+    $("body").append(app.templates.introOverlay());
+    $("#intro-overlay h1").textillate({
+      loop: true,
+      minDisplayTime: 500,
+      in: {
+        effect: 'fadeInUp',
+        shuffle: true
+      },
+      out: {
+        effect: 'fadeOutUp',
+        shuffle: true,
+        callback: function () {
+          $("#intro-overlay h1").hide();
+          $("#intro-overlay").fadeOut(function () {
+            $(this).remove();
+          });
+        }
+      }
+    });
+  };
+
+  app.displayLocationsList = function(locations){
+    $("#locations-list").html(app.templates.locations(locations));
+  };
+
+  app.displayMap = function(mapDOMElementID){
+    var map = L.map('starling-map',{
+      zoomControl: false,
+      attributionControl: false
+    }).setView([37.755817, -122.389932], 11);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        maxZoom: 18,
+        id: creds.id,
+        accessToken: creds.accessToken
+      }).addTo(map);
+
+    /*
+       map.on("click",function(e){
+       var id = eid();
+       var circle = L.circle(e.latlng, 100, {
+       weight:2,
+       color: '#f00',
+       fillOpacity: 0
+       }).addTo(map);
+
+       circle.on("click",function(e){
+       map.removeLayer(circle);
+       for (var i=0;i<app.locations.length;i++){
+       if (app.locations[i].mapObject===circle){
+       app.locations.splice(i,1);
+       break;
+       }
+       }
+       app.displayLocationsList(app.locations);
+       });
+
+       app.locations.push({id:id, latlng:e.latlng, radius: 100, mapObject: circle, title:"Title", notes:"notes"});
+       app.displayLocationsList(app.locations);
+
+       });
+       */
+
+    app.map = map;
+
+  };
+
+  app.init = function(){
+    app.displayMap('starling-atlas');
+    app.displayIntro();
+    app.listeners();
+  };
+
 
   window.app = app;
 
